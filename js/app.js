d006f835e4c1a08d04ae5481652f462b56af4ca4
Bookman <boocolin@38f9d361d56c.ant.amazon.com>
2019-11-20 22:31:16 -0800

    Added API & fixed frontend bugs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const REDIRECT_SUCCESS = 'https://imgflip.com/i/3h1wkg';
+
 class CaptivePortal {
   constructor(params) {
     this.errEl = params.errEl;
@@ -21,7 +23,6 @@ class CaptivePortal {
     this.photoEl.addEventListener('touchend', this.onPhotoMouseUp.bind(this));
     this.photoEl.addEventListener('touchmove', this.onPhotoMouseMove.bind(this));
 
-
     this.submitEl.onclick = this.onSubmit.bind(this);
   }
 
@@ -31,7 +32,7 @@ class CaptivePortal {
 
   onPhotoMouseMove() {
     if (this.coriePetting && Date.now() - this.coriePetting > 1500) {
-      this.passwordEl.value = atob(btoa('some password'));
+      this.passwordEl.value = atob(btoa('YOUR PASSWORD HERE'));
     }
   }
 
@@ -49,13 +50,61 @@ class CaptivePortal {
     }
   }
 
+  showError(msg) {
+    this.errEl.textContent = `Error: ${msg}`;
+    this.errEl.classList.remove('hide');
+    this.shake();
+  }
+
   onSubmit() {
     const pswd = this.passwordEl.value;
     if (pswd.length === 0) {
-      this.errEl.textContent = "Error: Please enter a wifi password";
-      this.errEl.classList.remove('hide');
-      this.shake();
+      this.showError('Please enter a wifi password');
+      return;
     }
+
+    // Grab Mac & AP from URL
+    const urlParams = new URLSearchParams(window.location.search);
+    if (!urlParams.has('id')) {
+      this.showError('You were not referred through captive portal. Please contact admin');
+      return;
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/auth_guest');
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.onload = () => {
+      if (xhr.status == 401) {
+        this.showError('Invalid Password')
+        return;
+      }
+
+      if (xhr.status == 503) {
+        this.showError('Server side issues authenticating for wifi');
+        return;
+      }
+
+      if (xhr.status != 200) {
+        this.showError('Something went wrong :\'');
+        return;
+      }
+
+      const resp = JSON.parse(xhr.responseText);
+      if (resp.status == "registered") {
+        window.location = REDIRECT_SUCCESS;
+      }
+    };
+
+    xhr.onerror = () => {
+      console.log(arguments);
+      alert("BOO");
+    };
+
+    xhr.send(JSON.stringify({
+      ap: urlParams.get('ap'),
+      mac: urlParams.get('id'),
+      password: pswd,
+    }));
 
     // todo(bookman): finish logic
   }
--- main.go
+++ main.go
@@ -1,7 +1,99 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/cobookman/unifi"
 )
 
+var (
+	ubiquitiUser = os.Getenv("user")
+	ubiquitiPass = os.Getenv("pass")
+	wifipass     = os.Getenv("wifipass")
+)
+
+// FileSystem custom file system handler
+type FileSystem struct {
+	fs http.FileSystem
+}
+
+// Open opens file
+func (fs FileSystem) Open(path string) (http.File, error) {
+	f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := fs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
+
+type AuthRequest struct {
+	Ap       string `json:"ap"`
+	Mac      string `json:"mac"`
+	Password string `json:"password"`
+}
+
+// http: //192.168.1.35/guest/s/default/?ap=74:83:c2:d6:c9:db&id=38:f9:d3:61:d5:6c&t=1574315384&url=http://fooz.com%2f&ssid=corcor+guest
+func authGuest(w http.ResponseWriter, r *http.Request) {
+	var authReq AuthRequest
+
+	err := json.NewDecoder(r.Body).Decode(&authReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if authReq.Password != wifipass {
+		http.Error(w, "{\"status\": \"Invalid Password\"}", http.StatusUnauthorized)
+		return
+	}
+
+	u := unifi.NewClient(ubiquitiUser, ubiquitiPass, "https://192.168.1.1:8443", "default", "5.12.22", true)
+	if err := u.Login(); err != nil {
+		http.Error(w, "Failed to connect to Admin Console", http.StatusInternalServerError)
+	}
+
+	guest := unifi.UnifiGuest{
+		Mac:     authReq.Mac,
+		Expires: 1 * 60 * 24, // 24 hours
+		Up:      1024 * 3,    // 3 Mibps
+		Down:    1024 * 35,   // 35 Mibps
+		Data:    1024 * 50,   // 50 GiB
+	}
+
+	if err := u.AuthGuest(guest); err != nil {
+		http.Error(w, "Failed to register client for Guest Wifi", http.StatusInternalServerError)
+	}
+
+	fmt.Fprintf(w, "{\"status\": \"registered\"}")
+}
+
+func main() {
+	port := flag.String("p", "80", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	// handle api calls
+	http.HandleFunc("/api/auth_guest", authGuest)
+
+	// handle static asset calls
+	fileServer := http.FileServer(FileSystem{http.Dir(*directory)})
+	http.Handle("/", http.StripPrefix(strings.TrimRight("/", "/"), fileServer))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
